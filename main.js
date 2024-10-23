d47d9b9477cbba4574d30424cdd15fadf7806467
Abdelhadi Alioui <abdelhadialioui2003@gmail.com>
2024-10-23 23:49:35 +0200

    Résolution d'un bug dans la partie contacte

--- main.js
+++ main.js
@@ -31,13 +31,13 @@ const sections = document.querySelectorAll('section[id]')
 const scrollActive = () =>{
     const scrollDown = window.scrollY
 
-            sections.forEach(current =>{
-            const scrollHeight = window.offsetHeight,
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
                 sectionTop = current.offsetTop - 58,
                 sectionId = current.getAttribute('id'),
                 sectionsClass = document.querySelector('.nav__list a[href*='+ sectionId + ']')
 
-            if(scrolldown > sectionTop && scrollDown <= sectionTop +sectionHeight){
+            if(scrolldown > sectionTop && scrollDown <= sectionTop + sectionHeight){
                 sectionsClass.classList.add('active-link')
             }else{
                 sectionsClass.classList.remove('active-link')
